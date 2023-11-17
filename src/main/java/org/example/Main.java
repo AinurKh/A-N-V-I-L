96e8d164da3f4e627d3bbbb850a810ec37f3879b
Khaibulov Ainur <ainur14.khaybulov@gmail.com>
2023-11-18 00:12:28 +0300

    Plus Name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
             System.out.println("Hello world!");
-
+            System.out.println("fffff");
+            System.out.println("AINURCHIK");
         }
     }
 }
